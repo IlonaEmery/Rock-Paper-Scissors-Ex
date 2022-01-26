40927ecc6343a9f1b7c488a294257c3a5ef41b58
IlonaEmery <51895151+IlonaEmery@users.noreply.github.com>
2022-01-26 18:21:26 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,22 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+
+# ASK FOR USER INPUT
+U = INPUT(Please choose one of: "Rock, Paper, Scissors?")
+
+print("user chose:",u)
+
+# VALIDATIONS
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+# FINAL RESULT
